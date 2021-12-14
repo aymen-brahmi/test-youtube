af93fbd63c0872757caaed3a22f02065e364e73c
aymen <aiimen.br@gmail.com>
2021-12-14 12:32:48 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Comments from './components/Comments';
+import Principal from './components/principal';
+import SuggestionVideos from './components/SuggestionVideos';
+import Navig from './components/Navig';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <div className='row'>
+        <Navig />
+      </div>
+      <div className='row'>
+        <Principal />
+        <SuggestionVideos />
+      </div>
+      <div className='row'>
+        <Comments />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
+
---
+++ src/components/principal.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const principal = () => {
+  return (
+    <div className='col-md-7'>
+      <div className='row'>
+        <iframe
+          title='react-summet'
+          width='756'
+          height='425'
+          src='https://www.youtube.com/embed/tO8qHlr6Wqg?list=PLNBNS7NRGKMHLTeH4qfD3F320GXfj97kc'
+          frameBorder='0'
+          allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+};
+
+export default principal;
